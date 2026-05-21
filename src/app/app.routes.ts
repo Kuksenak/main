3bd22380597ad3107aad25a30da908995cd97e6a
kuksenak <kuksenak@gmail.com>
2026-05-21 14:37:06 +0200

    update styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,15 +5,15 @@ import { Settings } from './_todo-features/settings/settings';
 
 export const routes: Routes = [
     { path: 'home', component: Home, canActivate: [authGuard] },
-    { path: 'settings', component: Settings, canActivate: [authGuard] },
+    // { path: 'settings', component: Settings, canActivate: [authGuard] },
     { path: 'server-error', redirectTo: 'home', pathMatch: 'full' },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     // { path: 'devices', component: Devices, canActivate: [authGuard] },
-    // {
-    //     path: 'mfa',
-    //     loadComponent: () =>
-    //         import('./features/mfa/mfa-list/mfa').then((m) => m.Mfa),
-    //     canActivate: [authGuard],
-    // },
+    {
+        path: 'settings',
+        loadComponent: () =>
+            import('./features/settings/settings').then((m) => m.Settings),
+        canActivate: [authGuard],
+    },
     // { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,14 +2,13 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthStore } from './_todo-core/auth/auth-store';
 import { Navbar } from './core/ui/navbar/navbar';
-import { PlayerBar } from './core/ui/player-bar/player-bar';
 import { environment } from '@environments/environment';
 import { SignalRService } from './_todo-core/realtime/signalr';
 import { NotificationService } from './_todo-core/notifications/notification-service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, PlayerBar],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
   // styleUrl: './app.scss'
 })
